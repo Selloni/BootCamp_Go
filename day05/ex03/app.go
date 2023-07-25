48f3284bfb3b78b70d66f3f53fe16e88aa3c3dae
Grandpa Trula <grandpat@si-d5.kzn.21-school.ru>
2023-07-25 18:35:47 +0300

    материал https://habr.com/ru/articles/561120/ нужно доразобрать рашение написать тесты, возможно полный размер диска вытащить

--- day05/ex03/app.go
+++ day05/ex03/app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	present "day05/ex02/struct"
+	"fmt"
+	"math"
 )
 
 func main() {
@@ -11,12 +13,38 @@ func main() {
 		present.Present{Value: 3, Size: 1},
 		present.Present{Value: 5, Size: 2},
 	}
-
+	fmt.Println(grabPresents(h, 4))
 }
 
-func grabPresents(item present.PresentHeap, size int) {
+func grabPresents(item present.PresentHeap, disk int) int {
+	var (
+		value []int
+		size  []int
+	)
+	for _, press := range item {
+		value = append(value, press.Value)
+		size = append(size, press.Size)
+	}
+
 	matrix := make([][]int, item.Len()+1)
 	for i := range matrix {
-		matrix[i] = make([]int, size+1)
+		matrix[i] = make([]int, disk+1)
 	}
+	for i := 0; i <= item.Len(); i++ {
+		for j := 0; j <= disk; j++ {
+			if i == 0 || j == 0 {
+				matrix[i][j] = 0
+			} else {
+				if size[i-1] > j {
+					matrix[i][j] = matrix[i-1][j]
+				} else {
+					prev := matrix[i-1][j]
+					tmp := value[i-1] + matrix[i-1][j-size[i-1]]
+					maxValue := math.Max(float64(prev), float64(tmp))
+					matrix[i][j] = int(maxValue)
+				}
+			}
+		}
+	}
+	return matrix[item.Len()][disk]
 }
