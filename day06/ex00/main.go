2839126dd49fcee9e753a20cf138380dc577d778
Grandpa Trula <grandpat@si-d5.kzn.21-school.ru>
2023-07-26 20:21:52 +0300

    amaizing_logo

--- day06/ex00/main.go
+++ day06/ex00/main.go
@@ -18,9 +18,22 @@ func main() {
 	fonColor := color.RGBA{0, 50, 100, 200}
 	draw.Draw(img, img.Bounds(), &image.Uniform{fonColor}, image.Pt(0, 0), draw.Src)
 	lineColor := color.RGBA{200, 30, 30, 255}
-	for x := 20; x < 300; x++ {
-		y := x/3 + 15
-		img.Set(x, y, lineColor)
+	for x := 50; x < 200; x++ {
+		y1 := x/3 + 25
+		y2 := x/3 + 50
+		y3 := x/3 + 75
+		img.Set(x, y1, lineColor)
+		img.Set(x, y2, lineColor)
+		img.Set(x, y3, lineColor)
+		y4 := x/3 + 125
+		y5 := x/3 + 150
+		y6 := x/3 + 175
+		img.Set(x+75, y4, lineColor)
+		img.Set(x+75, y5, lineColor)
+		img.Set(x+75, y6, lineColor)
+		for y := 250; y < 300; y++ {
+			img.Set(x+50, y, color.RGBA{0, 80, 8, 88})
+		}
 	}
 
 	png.Encode(file, img)
