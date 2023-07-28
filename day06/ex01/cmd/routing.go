321fd70a60110cd18a1db0898bff595209f322fe
Grandpa Trula <grandpat@si-d5.kzn.21-school.ru>
2023-07-28 23:25:50 +0300

    https://www.youtube.com/watch?v=YR9CDZqKS30
    подключаем бд

---
+++ day06/ex01/cmd/routing.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+	"text/template"
+	"web/ex01/pkg/db/psql"
+)
+
+// w: обращнеие к страничке r: параметр для передачи
+func homePage(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("../ui/html/home.html")
+	if err != nil {
+		log.Fatal("dont read home.html")
+	}
+	tmpl.Execute(w, nil)
+}
+
+func adminPanel(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("../ui/html/admin.html")
+	if err != nil {
+		log.Fatal("dont read admin.html")
+	}
+	tmpl.Execute(w, nil)
+}
+
+func savePost(w http.ResponseWriter, r *http.Request) {
+	login := r.FormValue("login")
+	pass := r.FormValue("pass")
+	text := r.FormValue("text")
+	if login == "admin" && pass == "admin" {
+		psql.OpenSql(text)
+	}
+
+}
+
+func handleRequest() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/admin/", adminPanel)
+	http.HandleFunc("/post/", savePost)
+	http.ListenAndServe(":8888", nil)
+}
--- day06/ex01/pkg/db/psql/psql.go
+++ day06/ex01/pkg/db/psql/psql.go
@@ -1,1 +1,39 @@
 package psql
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+)
+
+//
+//func OpenSql(text string) {
+//	connStr := "user=q password=q dbname=mypost sslmode=disable"
+//	db, err := sql.Open("postgres", connStr)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	defer db.Close()
+//
+//	result, err := db.Exec("insert into ", text)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	fmt.Println(result.RowsAffected())
+//}
+//
+
+type Client interface {
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	Begin(ctx context.Context) (pgx.Tx, error)
+	BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error)
+	BeginTxFunc(ctx context.Context, txOptions pgx.TxOptions, f func(pgx.Tx) error) error
+}
+
+func NewClient(ctx context.Context, name, pass, host, port, database string) {
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", name, pass, host, port, database)
+}
