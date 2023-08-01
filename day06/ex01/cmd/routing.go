ae6615aa9b734f549a7da142147401451928c17e
Grandpa Trula <grandpat@si-d5.kzn.21-school.ru>
2023-08-01 18:08:41 +0300

    не удаеться считать данные страницы, но зато работает база

--- day06/ex01/cmd/routing.go
+++ day06/ex01/cmd/routing.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"web/ex01/pkg/db/psql"
+
 	//_ "github.com/lib/pq"
 	"log"
 	"net/http"
@@ -26,18 +29,41 @@ func adminPanel(w http.ResponseWriter, r *http.Request) {
 }
 
 func savePost(w http.ResponseWriter, r *http.Request) {
-	login := r.FormValue("login")
-	pass := r.FormValue("pass")
-	text := r.FormValue("text")
-	if login == "admin" && pass == "admin" {
-		//psql.OpenSql(text)
-		fmt.Println(text)
+	///////////
+	cnf := psql.Config{
+		Name:     "grandpat",
+		Pass:     "grandpat",
+		Host:     "localhost",
+		Port:     "5432",
+		Database: "postgres",
 	}
+	psqlClient, err := psql.NewClient(context.Background(), cnf)
+	if err != nil {
+		log.Fatal("failed to connect to the database", err)
+	}
+	defer psqlClient.Close()
+	err = r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+	login := r.Form.Get("login")
+	pass := r.Form.Get("pass")
+	text := r.Form.Get("text")
+	//if login == "admin" && pass == "admin" {
+	fmt.Printf("ll %s, %s, %s", login, pass, text)
+	err = psql.InsertText(psqlClient, "222")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+	//}
 }
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/admin/", adminPanel)
 	http.HandleFunc("/post/", savePost)
+
 	http.ListenAndServe(":8888", nil)
 }
