bfe0a2ec8db1331d957bbf52995f303a31dff2bc
Grandpa Trula <grandpat@si-d5.kzn.21-school.ru>
2023-08-02 22:40:10 +0300

    осталось добавить логотип

--- day06/ex01/cmd/routing.go
+++ day06/ex01/cmd/routing.go
@@ -9,13 +9,14 @@ import (
 	"web/ex01/pkg/db/psql"
 )
 
+var flagPage uint = 1
+
 // w: обращнеие к страничке r: параметр для передачи
 func homePage(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("../ui/html/home.html")
 	if err != nil {
 		log.Fatal("dont read home.html")
 	}
-
 	cnf := psql.Config{
 		Name:     "grandpat",
 		Pass:     "grandpat",
@@ -28,9 +29,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("failed to connect to the database", err)
 	}
 	defer psqlClient.Close()
-	tmpl.Execute(w, psql.PostText(psqlClient))
-	//tmpl.Execute(w, nil)
-
+	tmpl.Execute(w, psql.PostText(psqlClient, flagPage))
 }
 
 func adminPanel(w http.ResponseWriter, r *http.Request) {
@@ -42,7 +41,6 @@ func adminPanel(w http.ResponseWriter, r *http.Request) {
 }
 
 func savePost(w http.ResponseWriter, r *http.Request) {
-
 	cnf := psql.Config{
 		Name:     "grandpat",
 		Pass:     "grandpat",
@@ -74,7 +72,6 @@ func savePost(w http.ResponseWriter, r *http.Request) {
 func showPost(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("../ui/html/articl.html")
 	vars := mux.Vars(r)
-	//fmt.Println(vars, 2)
 	cnf := psql.Config{
 		Name:     "grandpat",
 		Pass:     "grandpat",
@@ -91,12 +88,25 @@ func showPost(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func backPage(w http.ResponseWriter, r *http.Request) {
+	flagPage--
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func forwardPage(w http.ResponseWriter, r *http.Request) {
+	flagPage++
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
 func handleRequest() {
 	rout := mux.NewRouter()
 	rout.HandleFunc("/", homePage).Methods("GET")
 	rout.HandleFunc("/admin/", adminPanel).Methods("GET")
 	rout.HandleFunc("/post/", savePost).Methods("POST")
 	rout.HandleFunc("/post/{id:[0-9]+}", showPost).Methods("GET")
+	rout.HandleFunc("/back/", backPage).Methods("POST", "GET")
+	rout.HandleFunc("/forward/", forwardPage).Methods("POST", "GET")
+
 	http.Handle("/", rout)
 	http.ListenAndServe(":8888", nil)
 }
