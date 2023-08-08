6401dfd7e054eb84b10ad4aec7b180c3bfc6d2fb
Grandpa Trula <grandpat@un-d6.kzn.21-school.ru>
2023-08-08 22:52:58 +0300

    начинаме работать с многопоточностью

---
+++ day09/ex00/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	buff := []int{1, 1, 3, 7, 4, 5}
+	fmt.Print(sleepSort(buff))
+}
+
+func sleepSort(x []int) (sorti []int) {
+	ch := make(chan int)
+
+	for _, i := range x {
+		go forSleep(i, ch)
+	}
+	for i := 0; i < len(x); i++ {
+		sorti = append(sorti, <-ch)
+	}
+	return
+}
+
+func forSleep(x int, ch chan int) {
+	for i := 0; i < x; i++ {
+		time.Sleep(1 * time.Second)
+	}
+	ch <- x
+}
