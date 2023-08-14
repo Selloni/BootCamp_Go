47422659c78095000753261fa7dd7168bb5814c6
Grandpa Trula <grandpat@un-d6.kzn.21-school.ru>
2023-08-14 18:49:01 +0300

    напсиать сигнал cancel()

--- day09/ex01/main.go
+++ day09/ex01/main.go
@@ -5,19 +5,44 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	ch := make(chan string)
+	signalStop := make(chan os.Signal, 1)
 	urls := []string{
 		"https://example.com",
 		"https://google.com",
 		"https://github.com",
 		"https://ya.ru/?utm_referrer=https%3A%2F%2Fwww.google.com%2F",
 	}
+
+	signal.Notify(signalStop, os.Interrupt, os.Kill, syscall.SIGTERM)
+	go func() {
+		for {
+			<-signalStop
+			signal.Stop(signalStop)
+			log.Println("Signal type : ")
+			//_, cancel := context.Canceled
+			os.Exit(0)
+		}
+	}()
+
 	for _, url := range urls {
 		go crawlWeb(url, ch)
 		fmt.Println(<-ch)
+	}
+}
+
+func Signal(ch chan os.Signal) {
+	for {
+		<-ch
+		signal.Stop(ch)
+		//_, cancel := context.Canceled
+		os.Exit(0)
 	}
 }
 
