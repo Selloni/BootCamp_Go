9380c8f7c2103ab93a8fbb62f9d21f8585e4e400
Grandpa Trula <grandpat@un-d5.kzn.21-school.ru>
2023-08-26 12:43:03 +0300

    astikit bundler - мульти сборка

--- contest/main/B.go
+++ contest/main/B.go
@@ -37,3 +37,33 @@ func main() {
 		fmt.Fprintln(write, sum)
 	}
 }
+
+//func main() {
+//	read := bufio.NewReader(os.Stdin)
+//	write := bufio.NewWriter(os.Stdout)
+//	defer write.Flush()
+//	input, err := read.ReadString('\n')
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	//some := 0
+//	//fmt.Fscan(read, &some)
+//	goods := make(map[string]int)
+//	sl := strings.Fields(input)
+//	for _, i := range sl {
+//		goods[i] = goods[i] + 1
+//	}
+//	var sum int
+//	for v, count := range goods {
+//		vint, _ := strconv.Atoi(v)
+//		for i := count; i != 0; i-- {
+//			if i%3 == 0 {
+//				continue
+//			}
+//			sum = sum + vint
+//
+//		}
+//	}
+//	sumStr := strconv.Itoa(sum)
+//	write.WriteString(sumStr)
+//}
--- contest/main/C.go
+++ contest/main/C.go
@@ -9,10 +9,35 @@ import (
 func main() {
 	read := bufio.NewReader(os.Stdin)
 	write := bufio.NewWriter(os.Stdout)
-	some := 0
-	fmt.Fscan(read, &some)
-	//for i := 0; i < some; i++ {
-
-	//}
 	defer write.Flush()
+
+	var (
+		countLine int
+		countNum  int
+		//buff      []int
+		tmpBuff []int
+		lvl     int
+	)
+	fmt.Fscan(read, &countLine)
+	for i := 0; i < countLine; i++ {
+		list := make(map[int]int)
+		fmt.Fscan(read, &countNum)
+		for j := 0; j < countNum; j++ {
+			fmt.Fscan(read, &lvl)
+			list[j] = lvl
+		}
+
+		//for i, j := range buff {
+		//	tmp =
+		//	//tmpBuff = append(tmpBuff, i)
+		//}
+		fmt.Fprintln(write, tmpBuff)
+	}
+}
+
+func findPair(buff int, tmp int, write *bufio.Writer) {
+	if tmp == buff {
+		fmt.Fprintln(write, tmp, buff)
+	}
+
 }
